3ad07b2eafc7042a9fba0a7bc0184d52930353dc
Alister West <wesal@cpan.org>
2012-08-24 01:16:13 +0000

    Import of WESAL/RT-Extension-GroupBroadcast-0.1.1 from CPAN.
    gitpan-cpan-distribution: RT-Extension-GroupBroadcast
    gitpan-cpan-version:      0.1.1
    gitpan-cpan-path:         WESAL/RT-Extension-GroupBroadcast-0.1.1.tar.gz
    gitpan-cpan-author:       WESAL
    gitpan-cpan-maturity:     released

---
+++ lib/RT/Extension/GroupBroadcast.pm
@@ -0,0 +1,62 @@
+package RT::Extension::GroupBroadcast;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+RT::Extension::GroupBroadcast - Broadcast Messages via Email to existing RT groups.
+
+=head1 VERSION
+
+Version 0.1.1
+
+=cut
+
+our $VERSION = '0.1.1';
+
+
+=head1 SYNOPSIS
+
+This RT extension enables the sending of bulk email to a predefined RT group.
+
+    http://rt.example.com/Admin/GroupBroadcast.html
+
+=head1 AUTHOR
+
+Alister West, C<< <alister at alisterwest.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RT-Extension-GroupBroadcast>.
+I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc RT::Extension::GroupBroadcast
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/RT-Extension-GroupBroadcast/>
+
+=back
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Alister West.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1; # End of RT::Extension::GroupBroadcast
